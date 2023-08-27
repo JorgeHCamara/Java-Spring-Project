bfd23eacfaa4a67fc7c5f4eac2af9ba6d5ab796c
JorgeHCamara <jorgehcamara@hotmail.com>
2023-08-26 22:49:42 -0300

    chore: added thymeleaf to project

--- mp/src/main/java/br/com/fiap/mp/controller/HelloController.java
+++ mp/src/main/java/br/com/fiap/mp/controller/HelloController.java
@@ -3,13 +3,15 @@ package br.com.fiap.mp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
 	
 	@GetMapping
-	public String hello() {
+	public String hello(Model model) {
+		model.addAttribute("nome", "Jorge");
 		return "hello";
 	}
 	
